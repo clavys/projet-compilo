391e1f399483f468288d0d3c9e41a7ada84eb28f
reppert <william.reppert@etu.univ-nantes.fr>
2025-08-14 18:22:17 +0200

    + méta_compilateur

--- src/meta/meta_lexer.cpp
+++ src/meta/meta_lexer.cpp
@@ -8,36 +8,6 @@ namespace meta {
 MetaLexer::MetaLexer(const std::string& input)
     : source(input), pos(0), line(1), column(1) {}
 
-std::vector<Token> MetaLexer::tokenize() {
-    std::vector<Token> tokens;
-    while (pos < source.size()) {
-        skip_whitespace();
-
-        if (match("->")) tokens.push_back(make(TokenType::Arrow, "->", 2));
-        else if (match("|")) tokens.push_back(make(TokenType::Pipe));
-        else if (match("*")) tokens.push_back(make(TokenType::Star));
-        else if (match("+")) tokens.push_back(make(TokenType::Plus));
-        else if (match("(/")) tokens.push_back(make(TokenType::SlashOpen, "(/", 2));
-        else if (match("/)")) tokens.push_back(make(TokenType::SlashClose, "/)", 2));
-        else if (match("(")) tokens.push_back(make(TokenType::LParen));
-        else if (match(")")) tokens.push_back(make(TokenType::RParen));
-        else if (match("[")) tokens.push_back(make(TokenType::LBracket));
-        else if (match("]")) tokens.push_back(make(TokenType::RBracket));
-        else if (match(";")) tokens.push_back(make(TokenType::Semicolon));
-        else if (std::isalpha(peek())) tokens.push_back(lex_identifier());
-        else {
-            std::cerr << "Unknown character: " << peek() << "\n";
-            advance();
-        }
-    }
-    tokens.push_back(make(TokenType::EndOfFile, "", 0));
-    return tokens;
-}
-
-Token MetaLexer::make(TokenType type, const std::string& val, int len) {
-    return Token{type, val.empty() ? source.substr(pos - len, len) : val, line, column - len};
-}
-
 char MetaLexer::peek() const {
     return pos < source.size() ? source[pos] : '\0';
 }
